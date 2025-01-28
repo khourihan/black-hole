46d636e9b9b4f16f9b30d81f050f0410d168ac95
Killian Hourihan <kllnhrhn@gmail.com>
2025-01-28 13:14:16 -0800

    Add basic egui+winit+wgpu application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use input::InputManager;
+use winit::{event_loop::{ControlFlow, EventLoop}, keyboard::KeyCode};
+
+mod gui;
+mod state;
+mod app;
+mod input;
+
+struct Renderer {
+    info_window_open: bool,
+}
+
+impl app::Renderer for Renderer {
+    fn input(&mut self, input: &InputManager) {
+        self.info_window_open ^= input.key_pressed(KeyCode::KeyT);
+    }
+
+    fn render(&mut self, ctx: &mut app::RenderContext) {
+
+    }
+
+    fn gui(&mut self, ctx: &egui::Context) {
+        egui::Window::new("")
+            .open(&mut self.info_window_open)
+            .show(ctx, |ui| {
+
+            });
+    }
+}
+
+impl Default for Renderer {
+    fn default() -> Self {
+        Self {
+            info_window_open: true,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let renderer = Renderer::default();
+
+    let mut app = app::App::new(renderer);
+
+    event_loop.run_app(&mut app).expect("failed to run app.");
 }
---
+++ src/state.rs
@@ -0,0 +1,86 @@
+use winit::window::Window;
+
+use crate::gui::GuiRenderer;
+
+pub struct State {
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub surface_config: wgpu::SurfaceConfiguration,
+    pub surface: wgpu::Surface<'static>,
+    pub scale_factor: f32,
+    pub gui: GuiRenderer,
+}
+
+impl State {
+    pub async fn new(
+        instance: &wgpu::Instance,
+        surface: wgpu::Surface<'static>,
+        window: &Window,
+        width: u32,
+        height: u32,
+    ) -> Self {
+        let power_pref = wgpu::PowerPreference::default();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: power_pref,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .expect("failed to find an appropriate adapter.");
+
+        let features = wgpu::Features::empty();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: features,
+                    required_limits: Default::default(),
+                    memory_hints: Default::default(),
+                },
+                None,
+            )
+            .await
+            .expect("failed to create device.");
+
+        let swapchain_capabilities = surface.get_capabilities(&adapter);
+        let selected_format = wgpu::TextureFormat::Bgra8UnormSrgb;
+        let swapchain_format = swapchain_capabilities
+            .formats
+            .iter()
+            .find(|d| **d == selected_format)
+            .expect("failed to select proper surface texture format.");
+
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: *swapchain_format,
+            width,
+            height,
+            present_mode: wgpu::PresentMode::AutoVsync,
+            desired_maximum_frame_latency: 0,
+            alpha_mode: swapchain_capabilities.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &surface_config);
+
+        let gui = GuiRenderer::new(&device, surface_config.format, None, 1, window);
+
+        let scale_factor: f32 = 1.0;
+
+        Self {
+            device,
+            queue,
+            surface_config,
+            surface,
+            scale_factor,
+            gui,
+        }
+    }
+
+    pub fn resize_surface(&mut self, width: u32, height: u32) {
+        self.surface_config.width = width;
+        self.surface_config.height = height;
+        self.surface.configure(&self.device, &self.surface_config);
+    }
+}
